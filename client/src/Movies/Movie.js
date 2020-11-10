e81d1416154d558aa79ab92c51c0e5a72335efdb
Samantha Prewitt <67523846+Samanthap82@users.noreply.github.com>
2020-11-09 23:12:25 -0600

    finshed

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
-export default function Movie(props) {
+export default function Movie() {
   const [movie, setMovie] = useState();
-
-  let id = 1;
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
+  const { id } = useParams();
 
   useEffect(() => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`) // Study this endpoint with Postman
-      .then(response => {
+      .then((response) => {
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
+        setMovie(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
     // This effect should run every time time
     // the `id` changes... How could we do this?
-  }, []);
+  }, id);
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => { }
 
+  // const movie = movies.find(it => {
+  //   return it.td == id
+  // }) || {}
+
   if (!movie) {
-    return <div>Loading movie information...</div>;
+    return <div>(Loading movie information...)</div>;
   }
 
   const { title, director, metascore, stars } = movie;
@@ -42,7 +47,7 @@ export default function Movie(props) {
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {stars.map((star) => (
           <div key={star} className="movie-star">
             {star}
           </div>
